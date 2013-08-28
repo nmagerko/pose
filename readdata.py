0fd2b9c91de5b970a966d0e06707ec1769c1a83d
J0Nreynolds <j14reynolds@gmail.com>
2013-08-28 13:21:59 -0500

    readdata script added

---
+++ readdata.py
@@ -0,0 +1,38 @@
+###
+# Using the data provided from the output of getdata, 
+# this script splits the data into frames by the !!!
+# character pattern, followed by the depth and RGB
+# data, which is split by the $$$ pattern
+#
+# Jon Reynolds, Nick Magerko 2013
+###
+
+import sys
+
+filename = sys.argv[1]
+f = file(filename, 'r')
+rawdata = f.readlines()
+f.close()
+
+data = ""
+images = []
+depth = []
+RGB= []
+
+for line in rawdata:
+    data = data + line.strip()
+
+splitimages = data.split('!!!')
+for image in splitimages:
+    images.append(image)
+
+
+for sensordata in images:
+    if sensordata != "":
+        try:
+            depth.append(sensordata.split('$$$')[0])
+            RGB.append(sensordata.split('$$$')[1])
+        except IndexError:
+	    pass
+
+print(str(depth) + "\n" + str(RGB))
