Fixed issues with rotation and translation, but not application of either

calibrate_old.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import numpy as np
-import cv2, time, sys
+import cv2, time, sys, freenect
 
+# NOTE: This is no longer going to be used
 # Modified from: https://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html
 # Data checked with http://vision.in.tum.de/data/datasets/rgbd-dataset/file_formats
 
 dims = (9, 6) 					# 9x6 chessboard
-boards = 20						# number of boards to be collected
+boards = 5						# number of boards to be collected
 npoints = dims[0] * dims[1]		# Number of points on chessboard
 successes = 0					# Number of successful collections
 
@@ -14,8 +15,8 @@ successes = 0					# Number of successful collections
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((9*6,3), np.float32)
-objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+objectpoints = np.zeros((9*6,3), np.float32)
+objectpoints[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -35,9 +36,7 @@ while True and successes != boards:
 
 	if k%256 == 32:
 		# Capture an image
-		capture = cv2.VideoCapture(1)
-		# Get a frame while we have less than 8 successes
-		_, image = capture.read()
+		(image, _) = freenect.sync_get_video()
 		# Create a grayscale image
 		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 		# Attempt to find the chessboard corners
@@ -46,10 +45,10 @@ while True and successes != boards:
 		# If found, add object points, image points (after refining them)
 		if ret:
 			print("Found frame {0}".format(successes+1))
-			objpoints.append(objp)
+			objpoints.append(objectpoints)
 
 			cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
-			imgpoints.append(corners)
+			imgpoints.append(corners[0])
 
 	        # Draw and display the corners
 			cv2.drawChessboardCorners(image, dims, corners, True)
@@ -65,7 +64,7 @@ while True and successes != boards:
 cv2.destroyAllWindows()
 print("All frames found. Starting calibration....")
 
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(np.array(objectpoints), imgpoints, gray.shape[::-1],None,None)
 
 img = cv2.imread("output/calibration-images/calibration"+str(dims[0])+"x"+str(dims[1])+"-1.jpg")
 h,  w = img.shape[:2]

depth-to-world.py
@@ -1,74 +1,61 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from math import tan
 import cv2
 import freenect
 import time
-import pickle
 
-dims = (9, 6) 					# 9x6 chessboard
-boards = 1						# number of boards to be collected
-npoints = dims[0] * dims[1]		# Number of points on chessboard
+dims = (9, 6) 					# 9x6 chessboard (see images directory)
+npoints = dims[0] * dims[1]			# Number of points on chessboard
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((9*6,3), np.float32)
-objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+# 3d point in real world space
+objpoints = np.zeros( (np.prod(dims), 3), np.float32 )
+objpoints[:,:2] = np.indices(dims).T.reshape(-1, 2)
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-
-# Rotation and translation vectors
+# rotation and translation vectors
 rvec = []
 tvec = []
 
-fig = plt.figure()
+# list to store each point cloud generated
 clouds = []
 
-# Calibration output
-mtx = np.loadtxt("output/intrinsics" + str(dims[0])+"x"+str(dims[1]) + ".txt")
-dst = np.loadtxt("output/distortion" + str(dims[0])+"x"+str(dims[1]) + ".txt")
+# calibration output - from file
+mtx = np.loadtxt("output/intrinsics9x6.txt")
+dst = np.loadtxt("output/distortion9x6.txt")
 
-def CalculateRT():
-	successes = 0					# Number of successful collections
-	while True and successes != boards:
-		capture = None
+def CalculateRT(image):
+	# convert to grayscale
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# attempt to find the chessboard corners
+	ret, corners = cv2.findChessboardCorners(gray, dims)
 
-		# Capture an image
-		(image, _) = freenect.sync_get_video()
-		# Conver to grayscale
-		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		# Attempt to find the chessboard corners
-		ret, corners = cv2.findChessboardCorners(gray, dims)
+	# if found, refine the corners' location, and solve for rotation and translation
+	if ret:
+		cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
 
-		# If found, add object points, image points (after refining them)
-		if ret:
-			objpoints.append(objp)
+		# create matrix to match shape required by solvePnP
+		# 2d points in image plane.
+		imgpoints = corners.reshape(npoints, 2)
 
-			cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
-			imgpoints.append(corners)
-			successes += 1
+		ret, rvec, tvec = cv2.solvePnP(objpoints, imgpoints, mtx, dst)
 
-	# Create new imgpoints matrix to match shape required by solvePnP
-	imgpoints2 = np.zeros(shape=(npoints, 2))
-	for i in range(0, 54):
-		imgpoints2[i] = imgpoints[0][i][0]
+		# Calculate rotation matrix
+		rmtx = cv2.Rodrigues(rvec)[0]
 
-	ret, rvec, tvec = cv2.solvePnP(np.array(objpoints[0]), imgpoints2, mtx, dst)
-
-	# Calculate rotation matrix and create [R|t]
-	rmtx = cv2.Rodrigues(rvec)[0]
-
-	return rmtx, tvec
+		# return the rotation and translation
+		return rmtx, tvec
+	else:
+		# otherwise, return both as "Error"
+		return "Error", "Error"
 
 def RawDepthToMeters(depthValue):
 	# http://pille.iwr.uni-heidelberg.de/~kinect01/doc/classdescription.html#kinectcloud-section
 	return 1/(depthValue * (-0.0030711016) + 3.3309495161)
 
+# converts the depth data from the Kinect to meters
 def DepthToWorld(x, y, depthValue):
 
 	fx_d = mtx[0,0]
@@ -86,23 +73,39 @@ def DepthToWorld(x, y, depthValue):
 	return result
 
 def GenerateCloud():
-	print "Getting depth...",
-	(depth,_) = freenect.sync_get_depth()
-	print " \t \t \t OK"
-	print "Waiting for calibration object..."
-	rotate, translate = CalculateRT()
-	print "Rotation and translation calculated"
+	while (True):
+		image = None
+		depth = None
+		rotate = None
+		translate = None
+		# get a frame using libfreenect
+		(image, _) = freenect.sync_get_video()
+		(depth,_) = freenect.sync_get_depth()
+		rotate, translate = CalculateRT(image)
+		if rotate != "Error":
+			break;
+		else:
+			print "Grid not found"
+	print "Camera Center: "
+	print (np.dot(-np.transpose(rotate),translate))
+	print "Rotation: "
+	print rotate
+	print "Translation :"
+	print translate
 	# Collects 12288 points/frame
 	worldCoordinates = np.arange(36864, dtype = np.float64).reshape(96, 128, 3)
 	for i in range(0, 480, 5):
 		for j in range(0, 640, 5):
 			depthValue = depth[i,j]
 			if depthValue < 2047:
+				# if the depth value is small enough, convert it
+				# to depth in meters
 				values = DepthToWorld(i, j, depthValue)
 				worldCoordinates[i/5, j/5, 0] = values[0]
 				worldCoordinates[i/5, j/5, 1] = values[1]
 				worldCoordinates[i/5, j/5, 2] = values[2]
 			else:
+				# otherwise, assign the value to zero
 				worldCoordinates[i/5, j/5, 0] = 0
 				worldCoordinates[i/5, j/5, 1] = 0
 				worldCoordinates[i/5, j/5, 2] = 0
@@ -110,25 +113,20 @@ def GenerateCloud():
 	for row in worldCoordinates:
 		for point in row:
 			if str(point) != "[ 0.  0.  0.]":
+				# apply the rotation and translation to each x, y and z coordinate
 				point = rotate.dot(np.array(point).reshape(3, 1)) + translate
 				x.append(float(point[0]))
 				y.append(float(point[1]))
 				z.append(float(point[2]))
-
+	# append the x, y and z lists to clouds as a list of lists,
+	# creating a point cloud
 	clouds.append([x,y,z])
 
 print "\n" + "KINECT ONLINE \n"
-for i in range(0, 1):
-	if i == 0:
-		raw_input("\n" + "Press any key to the LAST frame")
-		GenerateCloud()
-	elif i > 0:
-		raw_input("Press any key to get another frame")
-		GenerateCloud()
-	else:
-		raw_input("\n" + "Press any key to get the first frame")
-		GenerateCloud()
+for i in range(0, 5):
+	GenerateCloud()
 
+# output point clouds so that meshlab can read them
 with open("output/clouds.asc", "w") as f:
 	for cloud in clouds:
 		for i in range(0, len(cloud[0])):