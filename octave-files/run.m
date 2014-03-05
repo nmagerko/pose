02f4cf0d34e87289d9fcef03fa09cad37e24f479
Nick Magerko <nmagerko@gmail.com>
2014-03-05 11:31:37 -0600

    Modifications made to RANSAC program - execution successful

--- octave-files/run.m
+++ octave-files/run.m
@@ -1,42 +1,50 @@
 % load point cloud into variable
-load ../output/cocoa_puffs.mat cloud;
+load ../output/bunny-door.mat cloud;
 
 % find the threshold by finding the maximum distance between any two points,
 % and taking five percent of this value
-t = max(pdist(cloud)) * 0.002;
+t = max(pdist(cloud))*0.05;
 
-inlier_points = {}
-iterations = 0;
 current_outliers = cloud;
 axis off
 hold on
-while (iterations < 5)
-	% get the first set of inliers using the transpose of the pointcloud and
-	% the above threshold
-	[M, ~, inliers] = ransacfitplane(transpose(current_outliers), t);
-
-	[current_outliers,ones(size(current_outliers,1),1)]*M;
+while (true)
+	% get the first set of inliers indicies using the transpose of the pointcloud and
+	% the above threshold (if there are enough points)
+	try
+		[M, ~, inliers] = ransacfitplane(transpose(current_outliers), t);
 	
-	% get the outliers
-	v = false(size(current_outliers, 1), 1);
-	v(inliers) = 1;
-	outliers = find(~v);
+		% get the new set of outlier indicies
+		v = false(size(current_outliers, 1), 1);
+		v(inliers) = 1;
+		outliers = find(~v);
 	
-	% get all of the inliers as points from the point cloud and do the same for
-	% the outlier points
-	inlier_points{iterations+1} = cloud(inliers, :);
-	outlier_points = cloud(outliers, :);
+		% get all of the inliers as points from the point cloud and do the same for
+		% the outliers
+		inlier_points = cloud(inliers, :);
+		outlier_points = cloud(outliers, :);
 
-	[x, y] = meshgrid([min(cloud(inliers, 1)), max(cloud(inliers, 1))],[min(cloud(inliers, 2)), max(cloud(inliers, 2))]);
+		% solve for the x and y components of the plane
+		[x, y] = meshgrid([min(cloud(inliers, 1)), max(cloud(inliers, 1))],[min(cloud(inliers, 2)), max(cloud(inliers, 2))]);
 
-	% M - 4x1 array of plane coefficients in the form
-	% b(1)*X + b(2)*Y +b(3)*Z + b(4) = 0 [from ransacfitplane]
-	z = -(x*M(1)+y*M(2)+M(4))/M(3);
+		% M is a 4x1 array of plane coefficients in the form
+		% b(1)*X + b(2)*Y +b(3)*Z + b(4) = 0 [from ransacfitplane]
+		% solve for the z component
+		z = -(x*M(1)+y*M(2)+M(4))/M(3);
 
-	mesh(x, y, z)
-	% uncomment to plot inliers on plane
-	% plot3(cloud(inliers, 1), cloud(inliers, 2), cloud(inliers, 3), 'xr');
-	current_outliers = outlier_points;
-	iterations = iterations + 1;
+		% plot the x, y, and z components as a mesh
+		mesh(x, y, z);
+		% plot the current inliers (red x's), along with the current outliers (blue o's)
+		plot3(cloud(inliers, 1), cloud(inliers, 2), cloud(inliers, 3), 'xr');
+		plot3(cloud(outliers, 1), cloud(outliers, 2), cloud(outliers, 3), 'ob');
+		% set the current outlier points as the input for the next iteration
+		current_outliers = outlier_points;
+		pause
+	catch
+		% if ransacfitplane doesn't have any more points to fit, exit
+		if (strcmp(lasterror.message, "too few points to fit plane"))
+			break;
+		endif
+	end
 endwhile
 hold off;
